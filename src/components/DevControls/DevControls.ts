9cc997f690a4a2df829e679db4db9491b385477e
Matija Kočevar <matija.kocev@gmail.com>
2024-02-01 02:13:56 +0100

    Dev controls functionality

--- src/components/DevControls/DevControls.ts
+++ src/components/DevControls/DevControls.ts
@@ -10,12 +10,54 @@ export class DevControls {
         this.props = props;
         this.godModeButton = document.querySelector('#god-mode-button');
         this.shieldsOnButton = document.querySelector('#shields-on-button');
+
+        this.init();
     }
 
     init = () => {
-        if (!this.isDevelopment) {
-            this.godModeButton?.style.display === 'none';
-            this.shieldsOnButton?.style.display === 'none';
+        if (this.godModeButton && this.shieldsOnButton) {
+            this.godModeButton.addEventListener(
+                'click',
+                this.onGodModeButtonClick
+            );
+            this.shieldsOnButton.addEventListener(
+                'click',
+                this.onShieldsOnButtonClick
+            );
+
+            if (!this.isDevelopment) {
+                this.godModeButton.style.display = 'none';
+                this.shieldsOnButton.style.display = 'none';
+            }
+
+            if (this.isDevelopment) {
+                this.godModeButton.style.display = 'block';
+                this.shieldsOnButton.style.display = 'block';
+
+                const { game } = this.props;
+
+                if (game.godMode) this.godModeButton.classList.add('active');
+                if (game.shieldsOn)
+                    this.shieldsOnButton.classList.add('active');
+            }
         }
+    };
+
+    onGodModeButtonClick = () => {
+        const { game } = this.props;
+
+        game.godMode = !game.godMode;
+
+        if (game.godMode) this.godModeButton?.classList.add('active');
+        else this.godModeButton?.classList.remove('active');
+    };
+
+    onShieldsOnButtonClick = () => {
+        const { game } = this.props;
+
+        game.shieldsOn = !game.shieldsOn;
+
+        if (game.shieldsOn) this.shieldsOnButton?.classList.add('active');
+        else this.shieldsOnButton?.classList.remove('active');
     };
 }
