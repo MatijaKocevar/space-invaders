d2b144ee3d5a32bb26b25b1b1de71403342e4d4a
Matija Kočevar <matija.kocev@gmail.com>
2024-02-02 01:18:10 +0100

    FIxed passive events

--- src/common/InputHandler.ts
+++ src/common/InputHandler.ts
@@ -17,45 +17,6 @@ export class InputHandler {
         this.createMobileControls();
     }
 
-    createMobileControls = () => {
-        if (this.mobileControls?.children.length) {
-            this.mobileControls.innerHTML = '';
-        }
-
-        const leftButton = document.createElement('button');
-        leftButton.id = 'left-button';
-        leftButton.innerHTML = '&lt;';
-        leftButton.addEventListener('touchstart', this.onLeftButtonTouchStart);
-        leftButton.addEventListener('touchend', this.onLeftButtonTouchEnd);
-        leftButton.addEventListener('mousedown', this.onLeftButtonTouchStart);
-        leftButton.addEventListener('mouseup', this.onLeftButtonTouchEnd);
-
-        const fireButton = document.createElement('button');
-        fireButton.id = 'fire-button';
-        fireButton.innerHTML = 'FIRE';
-        fireButton.addEventListener('touchstart', this.onFireButtonTouchStart);
-        fireButton.addEventListener('touchend', this.onFireButtonTouchEnd);
-        fireButton.addEventListener('mousedown', this.onFireButtonTouchStart);
-        fireButton.addEventListener('mouseup', this.onFireButtonTouchEnd);
-
-        const rightButton = document.createElement('button');
-        rightButton.id = 'right-button';
-        rightButton.innerHTML = '&gt;';
-        rightButton.addEventListener(
-            'touchstart',
-            this.onRightButtonTouchStart
-        );
-        rightButton.addEventListener('touchend', this.onRightButtonTouchEnd);
-        rightButton.addEventListener('mousedown', this.onRightButtonTouchStart);
-        rightButton.addEventListener('mouseup', this.onRightButtonTouchEnd);
-
-        this.mobileControls?.append(leftButton, fireButton, rightButton);
-
-        if (window.innerWidth < 600) {
-            this.mobileControls?.classList.add('show-mobile-controls');
-        }
-    };
-
     keydownHandler = (e: KeyboardEvent) => {
         e.preventDefault();
 
@@ -78,51 +39,77 @@ export class InputHandler {
         }
     };
 
-    onLeftButtonTouchStart = (e: Event) => {
-        console.log('left button touched');
+    createMobileControls = () => {
+        if (this.mobileControls?.children.length) {
+            this.mobileControls.innerHTML = '';
+        }
 
-        e.preventDefault();
+        const leftButton = document.createElement('button');
+        leftButton.id = 'left-button';
+        leftButton.innerHTML = '&lt;';
+        leftButton.addEventListener('touchstart', this.onLeftButtonTouchStart, {
+            passive: true,
+        });
+        leftButton.addEventListener('touchend', this.onLeftButtonTouchEnd, {
+            passive: true,
+        });
+        leftButton.addEventListener('mousedown', this.onLeftButtonTouchStart);
+        leftButton.addEventListener('mouseup', this.onLeftButtonTouchEnd);
 
+        const fireButton = document.createElement('button');
+        fireButton.id = 'fire-button';
+        fireButton.innerHTML = 'FIRE';
+        fireButton.addEventListener('touchstart', this.onFireButtonTouchStart, {
+            passive: true,
+        });
+        fireButton.addEventListener('touchend', this.onFireButtonTouchEnd, {
+            passive: true,
+        });
+        fireButton.addEventListener('mousedown', this.onFireButtonTouchStart);
+        fireButton.addEventListener('mouseup', this.onFireButtonTouchEnd);
+
+        const rightButton = document.createElement('button');
+        rightButton.id = 'right-button';
+        rightButton.innerHTML = '&gt;';
+        rightButton.addEventListener(
+            'touchstart',
+            this.onRightButtonTouchStart,
+            { passive: true }
+        );
+        rightButton.addEventListener('touchend', this.onRightButtonTouchEnd, {
+            passive: true,
+        });
+        rightButton.addEventListener('mousedown', this.onRightButtonTouchStart);
+        rightButton.addEventListener('mouseup', this.onRightButtonTouchEnd);
+
+        this.mobileControls?.append(leftButton, fireButton, rightButton);
+
+        if (window.innerWidth < 600) {
+            this.mobileControls?.classList.add('show-mobile-controls');
+        }
+    };
+
+    onLeftButtonTouchStart = () => {
         this.keys.push('KeyA');
     };
 
-    onRightButtonTouchStart = (e: Event) => {
-        console.log('right button touched');
-
-        e.preventDefault();
-
+    onRightButtonTouchStart = () => {
         this.keys.push('KeyD');
     };
 
-    onFireButtonTouchStart = (e: Event) => {
-        console.log('fire button touched');
-
-        e.preventDefault();
-
+    onFireButtonTouchStart = () => {
         this.keys.push('Enter');
     };
 
-    onLeftButtonTouchEnd = (e: Event) => {
-        console.log('left button released');
-
-        e.preventDefault();
-
+    onLeftButtonTouchEnd = () => {
         this.keys.splice(this.keys.indexOf('KeyA'), 1);
     };
 
-    onRightButtonTouchEnd = (e: Event) => {
-        console.log('right button released');
-
-        e.preventDefault();
-
+    onRightButtonTouchEnd = () => {
         this.keys.splice(this.keys.indexOf('KeyD'), 1);
     };
 
-    onFireButtonTouchEnd = (e: Event) => {
-        console.log('fire button released');
-
-        e.preventDefault();
-
+    onFireButtonTouchEnd = () => {
         this.keys.splice(this.keys.indexOf('Enter'), 1);
     };
 
